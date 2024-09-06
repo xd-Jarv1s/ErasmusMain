3fd14f14dfdad81c0eb323232878d9671760e3c2
xd-Jarv1s <116846800+xd-Jarv1s@users.noreply.github.com>
2024-09-06 15:45:51 +0200

    Update bot.py

--- TelegramBot/bot.py
+++ TelegramBot/bot.py
@@ -1,21 +1,58 @@
-from pathlib import Path
+import json
+import re
+import requests
 
-import scrapy
+# Load the JSON file
+with open('dataClean.json', 'r') as file:
+    data = json.load(file)
 
+# Access the specific data
+specific_data = data[0]["data"][15]
+# print(specific_data)
 
-class QuotesSpider(scrapy.Spider):
-    name = "quotes"
+# Define regex patterns
+date_pattern = r'"Datum":"(\d{2}\.\d{2}\.\d{4})"'
+slug_pattern = r'"slug":"([^"]+)"'
+id_pattern = r'"_id":"([^"]+)"'
+pattern_title = r'"title":"([a-zA-Z\s]+)"'
 
-    def start_requests(self):
-        urls = [
-            "https://quotes.toscrape.com/page/1/",
-            "https://quotes.toscrape.com/page/2/",
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+# Extract data based on patterns
+dates = re.findall(date_pattern, specific_data)
+slugs = re.findall(slug_pattern, specific_data)
+ids = re.findall(id_pattern, specific_data)
+titles = re.findall(pattern_title, specific_data)
 
-    def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f"quotes-{page}.html"
-        Path(filename).write_bytes(response.body)
-        self.log(f"Saved file {filename}")
+# Ensure all lists have the same length
+min_length = min(len(dates), len(slugs), len(ids), len(titles))
+
+# Initialize index
+index = 0
+
+# Use a while loop to process each set of values
+while index < min_length:
+    date = dates[index]
+    slug = slugs[index]
+    id_ = ids[index]
+    title = titles[index]
+
+    # Construct the output URL
+    link = f"https://frey-tag.at/kalender/{slug}?s={id_}"
+
+    # Print the combined results and the link
+    print(f"Title: {title}")
+    print(f"Date: {date}")
+    print(f"Link: {link}")
+    print("-----------")
+
+    # Increment the index
+    index += 1
+
+    # Your Telegram bot token
+    token = '7162097876:AAE27cvUGt6tUzuX3NI9VoNnoUsbNYYnBUM'
+    method = "sendMessage"
+
+    # Make a POST request to the Telegram API
+    response = requests.post(
+        url='https://api.telegram.org/bot{0}/{1}'.format(token, method),
+        data={'chat_id': -1002230118853, 'text': 'hello friend'}
+    ).json()
